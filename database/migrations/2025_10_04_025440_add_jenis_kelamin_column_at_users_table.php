53941dda3245ae6bcc51d13d8c1f968ffb6ece82
Juang Salaz Prabowo <juang.salaz.prabowo@gmail.com>
2025-10-04 10:05:19 +0700

    update

---
+++ database/migrations/2025_10_04_025440_add_jenis_kelamin_column_at_users_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer('jenis_kelamin')->nullable(); // 1 = Laki-laki, 2 = Perempuan
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->drop('janis_kelamin');
+        });
+    }
+};
